73ee9ccad41a4533d2bbcf1bf8d4320b1a0645d1
Fluff <proffitt.m.asidrain@gmail.com>
2022-04-03 20:53:46 +0100

    End of section 10 (No gallery)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -34,6 +35,21 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return _mapper.Map<MemberDto>(await _userRepository.GetMemberByUsernameAsync(username));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
---
+++ Client/src/app/_interceptors/loading.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BusyService } from '../_services/busy.service';
+import { delay, finalize } from 'rxjs/operators';
+import random, { Random } from 'random';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private busySerivce: BusyService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.busySerivce.busy();
+    return next.handle(request).pipe(
+      delay(random.int(100, 320)),
+      finalize(() => {
+        this.busySerivce.idle();
+      })
+    );
+  }
+}
